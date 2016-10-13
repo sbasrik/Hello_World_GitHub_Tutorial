309f61097e5fc89d8c5867bacb19a12f9f59bfcf
ergun1017 <erdogmusergun@gmail.com>
2016-10-13 18:41:48 +0300

    iki sayı giriliyor artık bee.

--- Hello.c
+++ Hello.c
@@ -3,10 +3,13 @@
 int main()
 {
 	int a = 0;
+	int b = 0;
 
 	printf("Lütfen bir sayı gir : \n");
 	scanf("%d", &a);
-	printf("Girilen Sayi: %d\n", a);
+	printf("Lütfen bir sayı daha gir : \n");
+	scanf("%d", &b);
+	printf("Girilen İki  sayı bunlar %d %d\n", a, b);
 
 
 	return 0;
