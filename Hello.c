d3bddce65e0052263ed14fb887b666fc0bb331ce
Mahmut Karaca <mahmutkaraca@Mahmuts-MacBook-Air.local>
2016-10-13 18:39:03 +0300

    Yeni ozellik eklendi - Artik girilen sayi iki ile carpiliyor.

--- Hello.c
+++ Hello.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+int ikiilecarp(int a) {
+	return a*2;
+}
+
 int main()
 {
 	int a = 0;
 
 	printf("Lütfen bir sayı gir : \n");
 	scanf("%d", &a);
-	printf("Girilen Sayi: %d\n", a);
+	printf("Girilen Sayinin iki kati: %d\n", ikiilecarp(a));
 
 
 	return 0;
